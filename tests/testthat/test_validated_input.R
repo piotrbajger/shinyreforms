b1a80b6ffaf2e7eddcd08ebc3dedcdaa995d550a
Piotr Bajger <piotr.bajger@hotmail.com>
2020-02-11 00:47:24 +0100

    Add unit test for radioButtons

--- tests/testthat/test_validated_input.R
+++ tests/testthat/test_validated_input.R
@@ -91,3 +91,22 @@ test_that("Validate shiny::passwordInput", {
     expect_true(grepl("dummy", toString(newInput)))
     expect_true(grepl("shinyreforms-validation", toString(newInput)))
 })
+
+
+test_that("Validate shiny::radioButtons", {
+    testInput <- shiny::radioButtons(
+        "input_password",
+        label="Test",
+        choices=list(A=1, B=2, C=3)
+    )
+
+    newInput <- validatedInput(
+        testInput,
+        helpText="dummy",
+        validators=c(.testValidator)
+    )
+
+    expect_equal(length(attr(newInput, "validators")), 1)
+    expect_true(grepl("dummy", toString(newInput)))
+    expect_true(grepl("shinyreforms-validation", toString(newInput)))
+})
